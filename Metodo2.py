f8ed68480a6a66515abf49973497151aed78d4f1
Vianette-jg <vianettejorgegarcia@gmail.com>
2024-10-12 12:53:03 -0600

    Add files via upload

---
+++ Metodo2.py
@@ -0,0 +1,6 @@
+lista=[1,5,6,89,77,0,10,2,10,45,3,76,12,7]
+n=int(input("Ingresa el valor que quieras buscar:"))
+if n in lista:
+    print("se encontro en la lista")
+else:
+    print(" no se encontro en la lista")
